f0bc84cc6b9b80ba00ac6a6d490f7ea4ab635d16
Kabi4 <hykukku@gmail.com>
2020-12-31 09:01:26 +0530

    String Recap

---
+++ 04.StartingWithStrings/1.StartWithString.rb
@@ -0,0 +1,26 @@
+puts "This is a string"
+
+puts "String can consist of any characters @ #$@!($@!%*(#&(!@)(@!*#(%>?{}" ' '""
+
+puts "empty string can also be possible"
+
+puts " "
+
+puts " ".length
+
+puts "".length #0 length
+
+space = "undefined"
+life = "mortal"
+
+puts space,life
+
+#they all come from same class String
+
+puts space.class
+puts life.class
+
+#New way to create string with class
+const newString = String.new("Whatever the hell you want to make string!")
+
+p newString
---
+++ 04.StartingWithStrings/readme.md
@@ -0,0 +1,7 @@
+# Starting With Strings
+ ## String Recap
+  - String can be easily created by covering anything in `" "`.
+  - It can be empty too.
+  - All string have class `String`.
+  - By `String.new("String Here")` we can also create a String.
+  - `"a string".class` = `String`
