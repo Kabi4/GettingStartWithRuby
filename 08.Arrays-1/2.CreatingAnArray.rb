042d5bcc6d6b94a520391c2ba1967a82aa64aebd
Kabi4 <hykukku@gmail.com>
2021-01-05 17:56:32 +0530

    Create a array

---
+++ 08.Arrays-1/2.CreatingAnArray.rb
@@ -0,0 +1,23 @@
+numbers = [1,2,3,4,5,6,7,8] #create an array with 8 number these can be any number or object
+
+p numbers # [1,2,3,4,5,6,7,8]
+
+puts numbers #It print each object in separate lines
+
+toys = ["bear","transformer","superstrikers","swordman"]
+
+p toys # ["bear","transformer","superstrikers","swordman"]
+
+randomArray = [4,true,"hello",10.233]
+
+p randomArray # [4,true,"hello",10.233]
+
+registration = [true,true,true,true,false,false,true]
+
+p registration # [true,true,true,true,false,false,true]
+
+#array can be stored in another araay because it itself a object 
+
+a = [["sally",24,true],["simon",21,true],["sunny",19,false]]
+#Array within the array
+p a #[["sally",24,true],["simon",21,true],["sunny",19,false]]
--- 08.Arrays-1/readme.md
+++ 08.Arrays-1/readme.md
@@ -8,3 +8,9 @@
   - `[]` is a empty array.
   - `[].class` gives the class of array that is `Array`.
   - They can also be assign to a variable.
+
+ ## Creating an array
+  - An object can be created easily by the adding object inside `[]`.
+  - The object in array are separated by `,`.Ex: `[1,2,3,4,5,6]`.
+  - Array can be of any type of object.Even the object doesn't need to be same.Ex:`[4,true,"hello",10.233]`.
+  - Array is collections of objects and it itself a object so an array can be collections of arrays itself.Ex:`[["paid",true],["paid",false]]`.
