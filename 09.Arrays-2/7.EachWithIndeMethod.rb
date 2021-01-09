2b4c15f045a1a68ae889ee172054829e1db27cc9
Kabi4 <hykukku@gmail.com>
2021-01-09 21:00:23 +0530

    Each With index

--- 09.Arrays-2/7.EachWithIndeMethod.rb
+++ 09.Arrays-2/7.EachWithIndeMethod.rb
@@ -3,3 +3,10 @@ winnersInOrder = ["Joe","Bob","Harley"]
 winnersInOrder.each_with_index do |name,position|#accessing the index with second variable
     p "#{name} is qualified fo next round with #{position + 1} position in this round."
 end
+
+
+##Challanges if you want
+
+#[1,2,3,4,5] write programme to give us the sum of all the element with thier index position
+
+#[-1,2,1,2,5,7,3] write a programme that print element and its index position which if and only if the index position is less than the element value
