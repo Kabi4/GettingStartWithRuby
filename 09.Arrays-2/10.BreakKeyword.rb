2ed1e4ed2eb2a23bc66539f2890b06f6fc2f4713
Kabi4 <hykukku@gmail.com>
2021-01-10 10:09:36 +0530

    Break Keyword

--- 09.Arrays-2/10.BreakKeyword.rb
+++ 09.Arrays-2/10.BreakKeyword.rb
@@ -2,16 +2,20 @@ mine = ["pyrite","pyrite","pyrite","pyrite","pyrite","gold","pyrite","pyrite","p
 
 mine.each do |element|
     if element=="gold"
-        p "Found real gold dig here!"
+        print "Found real gold dig here! "
+        p element
         break #when the gold is found this keyword is gonna break the loop stop  executing the loop in it it is declared
     end
+    p element
 end
 
 nums = [1,2,3,4,5,'6',7,8]
 
 nums.each do |num|
     if !num.is_a?(Integer)
-        p "Find a #{num.class} type value in the nums array!"
+        print "Find a #{num.class} type value in the nums array! "
+        p num
         break
     end
+    p num
 end
