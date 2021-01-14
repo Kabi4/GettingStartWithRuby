f9d82505898362d98d2bb16e35ce5b30982d4728
Kabi4 <hykukku@gmail.com>
2021-01-14 21:13:35 +0530

    All and any Method

--- 11.Array-BONUS/3.anyAndallMethod.rb
+++ 11.Array-BONUS/3.anyAndallMethod.rb
@@ -1,5 +1,5 @@
 arr = [1,2,3,4,5,6,0]
 
-p arr.any? { |ele| ele.even? }
+p arr.any? { |ele| ele.even? } #returns true of even one element yields true
 
-p arr.all? { |ele| ele!=0 }
+p arr.all? { |ele| ele!=0 } #returns true of all elements gives true
