4e3d5cafeb13212bd69691d32fdeaa92eecadc16
Kabi4 <hykukku@gmail.com>
2021-01-30 20:03:35 +0530

    Modules With Indentical Methods

---
+++ 19.ModulesAndMixin/MODULES/Area.rb
@@ -0,0 +1,13 @@
+module Area
+    PI = 3.14
+    def self.area_square(side)
+        side*side
+    end
+    def self.area_rectangle(legnth,breath)
+        legnth*breath
+    end
+    def self.area_circle(r)
+        PI*r*r
+    end
+end
+
